3cc95034782bcd6c00630d76c5f7e0645e10477d
Erdenedalai <erdenedalai.er@novelsoft.mn>
2024-01-03 18:26:31 +0800

    reigister and login

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { BodyComponent } from '../body/body.component';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, SidebarComponent, BodyComponent],
+  imports: [NavbarComponent, SidebarComponent, BodyComponent, LoginComponent, RegisterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
